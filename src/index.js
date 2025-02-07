646f479418a144376a95cd7fd2ed9f776c491872
bima-taruna <bimataruna21@gmail.com>
2025-02-07 22:03:29 +0700

    test gameboard and gameboard view on index

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 if (process.env.NODE_ENV !== "production") {
   console.log("Looks like we are in development mode!");
 }
+const Gameboard = require("../src/js/models/gameboard.js");
+const GameboardView = require("../src/js/ui/gameboardView.js");
+
+const gameboard = new Gameboard();
+const gameboardView = new GameboardView(
+  gameboard,
+  document.getElementById("gameboard-container")
+);
