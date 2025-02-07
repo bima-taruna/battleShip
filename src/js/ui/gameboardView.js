dac9e4a4617bf6a2bbefa7b8b9af37e800884b48
bima-taruna <bimataruna21@gmail.com>
2025-02-07 21:50:55 +0700

    feat(gameboardView) : add render method

--- src/js/ui/gameboardView.js
+++ src/js/ui/gameboardView.js
@@ -4,5 +4,29 @@ class GameboardView {
     this.container = containerElement;
   }
 
-  renderBoard() {}
+  renderBoard() {
+    this.container.innerHTML = "";
+    for (let x = 0; x < 10; x++) {
+      const row = document.createElement("div");
+      row.classList.add("row");
+
+      for (let y = 0; y < 10; y++) {
+        const cell = document.createElement("div");
+        cell.classList.add("cell");
+
+        cell.dataset.x = x;
+        cell.dataset.y = y;
+        cell.textContent = this.gameboard.board[x][y] || "";
+
+        if (this.gameboard.board[x][y] === "H") {
+          cell.classList.add("hit");
+        } else if (this.gameboard.board[x][y] === "M") {
+          cell.classList.add("miss");
+        }
+
+        row.appendChild(cell);
+      }
+      this.container.appendChild(row);
+    }
+  }
 }
