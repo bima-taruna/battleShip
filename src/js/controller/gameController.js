141143922cfbf11aba49bf85880d9291295f6d0e
bima-taruna <bimataruna21@gmail.com>
2025-02-18 19:05:07 +0700

    <feat>(gameController) : add update board views method

--- src/js/controller/gameController.js
+++ src/js/controller/gameController.js
@@ -5,11 +5,25 @@ class GameController {
     this.player1GameboardView = player1GameboardView;
     this.player2GameboardView = player2GameboardView;
     this.currentPlayer = player1;
-    this.phase = "setup";
+    this.phase = "playing";
   }
 
-  switchPlayer() {
+  switchTurn() {
     this.currentPlayer =
       this.currentPlayer === this.player1 ? this.player2 : this.player1;
+  }
+
+  attack(x, y) {
+    if (this.phase !== "playing") {
+      return;
+    }
+    const opponent =
+      this.currentPlayer === this.player1 ? this.player2 : this.player1;
+    opponent.gameboard.receiveAttack(x, y);
+  }
+
+  updateBoardViews() {
+    this.player1GameboardView.updateBoard();
+    this.player2GameboardView.updateBoard();
   }
 }
