50473a1d7f9f0a92eb9a879d3b4e960634f13b1c
bima-taruna <bimataruna21@gmail.com>
2025-02-18 22:11:49 +0700

    feat(gameController) : enhance turn switching and attack logic, add ship placement method

--- src/js/controller/gameController.js
+++ src/js/controller/gameController.js
@@ -5,12 +5,16 @@ class GameController {
     this.player1GameboardView = player1GameboardView;
     this.player2GameboardView = player2GameboardView;
     this.currentPlayer = player1;
+    // this.currentGameboardView = player1GameboardView;
     this.phase = "playing";
   }
 
   switchTurn() {
     this.currentPlayer =
       this.currentPlayer === this.player1 ? this.player2 : this.player1;
+    // this.currentGameboardView = this.currentPlayer === this.player1 ? this.player1GameboardView : this.player2GameboardView;
+
+    console.log(`${this.currentPlayer.name}'s turn`);
   }
 
   attack(x, y) {
@@ -19,14 +23,26 @@ class GameController {
     }
     const opponent =
       this.currentPlayer === this.player1 ? this.player2 : this.player1;
+    if (
+      opponent.gameboard.board[x][y] === "H" ||
+      opponent.gameboard.board[x][y] === "M"
+    ) {
+      return;
+    }
     opponent.gameboard.receiveAttack(x, y);
     this.updateBoardViews();
+    console.log(`${this.currentPlayer.name} attacks ${x}, ${y}`);
     if (opponent.gameboard.isShipsSunk()) {
       console.log(`${this.currentPlayer.name} wins!`);
       this.endGame();
     } else {
       this.switchTurn();
     }
+  }
+
+  placeShip(player, x, y, ship) {
+    player.gameboard.placeShip(x, y, ship);
+    this.updateBoardViews();
   }
 
   updateBoardViews() {
